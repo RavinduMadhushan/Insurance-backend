3fc089aed028f7e5536b3848952bba152cba1034
Tharindu Dilshan Kulasinghe <dilshantharinduk@gmail.com>
2019-09-16 17:24:09 +0530

    Update config.js

--- routes/config.js
+++ routes/config.js
@@ -3,14 +3,15 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "mysql",
-  database: "insurance"
+  password: "",
+  database: "insurance_system"
 });
 
 connection.connect(function(err) {
   if (!err) {
     console.log("Database is connected");
   } else {
+    console.log(err);
     console.log("Error while connecting with database");
   }
 });
